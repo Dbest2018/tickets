1ee18b7a864418aadcab76d9f4783bf2aa3d0a19
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-10 01:31:04 +0000

    [feat] Add error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const userRoutes = require("./routes/userRoutes");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the support desk api" });
 });
 
 app.use("/api/users", userRoutes);
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
