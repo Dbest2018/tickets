4108b4a777c4dc5d390e17523f40c45e2571cccb
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-12 21:45:51 +0000

    [feat] Dispatch login action with redux

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -7,6 +9,12 @@ function Login() {
     password: "",
   });
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const handleChange = (e) => {
     setFormData((prevForm) => ({
@@ -17,6 +25,13 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
   return (
     <>
