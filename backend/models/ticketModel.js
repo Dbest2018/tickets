0c0e1765ec23b393e2781ca5fc8ff3f331550def
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-12 23:45:24 +0000

    [feat] Add ticket model

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectID,
+      required: [true, "Please add a name"],
+      ref: "User",
+    },
+    product: {
+      type: String,
+      required: [true, "Please select a product"],
+      enum: ["iPhone", "Macbook Pro", "iMac", "iPad"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please describe the issue"],
+    },
+    status: {
+      type: String,
+      enum: ["new", "open", "closed"],
+      default: "new",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Ticket", ticketSchema);
