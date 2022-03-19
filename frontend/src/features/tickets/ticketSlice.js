0ab00ba1bc781779620e0bd18c33a06ca889dd1d
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-19 10:40:58 +0000

    [feat] Fetch tickets from the backend

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -29,6 +29,25 @@ export const createTicket = createAsyncThunk(
   }
 );
 
+export const getTickets = createAsyncThunk(
+  "tickets/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.getTickets(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const ticketSlice = createSlice({
   name: "ticket",
   initialState,
@@ -40,9 +59,10 @@ export const ticketSlice = createSlice({
       .addCase(createTicket.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(createTicket.fulfilled, (state) => {
+      .addCase(createTicket.fulfilled, (state, action) => {
         state.isLoading = false;
         state.isSuccess = true;
+        state.tickets = action.payload;
       })
       .addCase(createTicket.rejected, (state, action) => {
         state.isLoading = false;
