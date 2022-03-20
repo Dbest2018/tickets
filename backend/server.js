fe227756e40ce9cb3828b50cb2249dab912b8a28
Shittu Ibrahim <shit2wice@gmail.com>
2022-03-20 21:30:19 +0000

    [chore] Set build folder as static

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
@@ -14,11 +15,22 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the support desk api" });
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/tickets", ticketRoutes);
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the support desk api" });
+  });
+}
+
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
